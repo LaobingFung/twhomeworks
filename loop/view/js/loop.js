018ea75f3f7787e11e3714bade5217ebddbeb631
LaoBing_Fung <FLB_B401@163.com>
2018-11-26 23:14:09 +0800

    output the 9*9 multiplication table

--- loop/view/js/loop.js
+++ loop/view/js/loop.js
@@ -7,3 +7,16 @@ function oddAndEvenFrom0To20() {
     }
   }
 }
+
+function multiplicationTable() {
+  for (let i = 1; i <= 9; i++) {
+    let oneLineInTable = "";
+    for (let j = 1; j <= i; j++) {
+      let result = i * j;
+      oneLineInTable += i + "*" + j + "=" + result + " ";
+    }
+    console.log(oneLineInTable);
+  }
+}
+oddAndEvenFrom0To20();
+multiplicationTable();
