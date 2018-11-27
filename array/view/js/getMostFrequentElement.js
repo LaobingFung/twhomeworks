d092a01cf96ec8e670c526f3b072d5bd4c35f4cf
LaoBing_Fung <FLB_B401@163.com>
2018-11-27 10:40:20 +0800

    fix bug to distinguish number and string of number

--- array/view/js/getMostFrequentElement.js
+++ array/view/js/getMostFrequentElement.js
@@ -1,17 +1,16 @@
 const A = [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
 
 function getMostFrequentElement(array) {
-  let elements = {};
-  let elementsCount = array.length;
+  let elements = new Map();
   let mostFrequentElement = null;
   let mostFrequentElementCount = 0;
-  for (let i = 0; i < elementsCount; i++) {
-    if (!elements[array[i]]) {
-      elements[array[i]] = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (!elements.has(array[i])) {
+      elements.set(array[i], 0);
     }
-    elements[array[i]]++;
-    if (elements[array[i]] > mostFrequentElementCount) {
-      mostFrequentElementCount = elements[array[i]];
+    elements.set(array[i], elements.get(array[i]) + 1);
+    if (elements.get(array[i]) > mostFrequentElementCount) {
+      mostFrequentElementCount = elements.get(array[i]);
       mostFrequentElement = array[i];
     }
   }
