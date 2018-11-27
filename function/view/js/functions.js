916b9ca94d79de0719a06ea3edf660e3ba195ebf
LaoBing_Fung <FLB_B401@163.com>
2018-11-27 19:51:35 +0800

    format functions.js by CodeFormatter

--- function/view/js/functions.js
+++ function/view/js/functions.js
@@ -1,7 +1,7 @@
 function revertString(message) {
-    let reverted = "";
-    for (let i = 0; i < message.length; i++){
-        reverted += message[message.length - 1 - i];
-    }
-    return reverted;
+  let reverted = "";
+  for (let i = 0; i < message.length; i++) {
+    reverted += message[message.length - 1 - i];
+  }
+  return reverted;
 }
